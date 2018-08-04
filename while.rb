c4e710b89976449b70a909b626fb573856436b2e
Magnus Everö <magnusevero@MagnusEsMacBook.lan>
2018-08-04 20:40:46 +0200

    while example

---
+++ while.rb
@@ -0,0 +1,12 @@
+# Some counting with a while.
+a = 0
+while a < 15
+    print a, " "
+    print "\n"
+    if a == 10 then
+        print "made it to ten!!"
+    end
+    a = a + 1
+end
+print "All done!"
+print "\n"
