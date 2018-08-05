725424bcf2c68eb6eff5a76caf41f8c95e086c44
Magnus Everö <magnusevero@MagnusEsMacBook.lan>
2018-08-05 11:26:51 +0200

    Corrected syntax bug

--- tc_simple_number.rb
+++ tc_simple_number.rb
@@ -1,15 +1,13 @@
-# File:  tc_simple_number.rb
-
-require_relative "simple_number"
+require_relative "calculator"
 require "test/unit"
  
-class TestSimpleNumber < Test::Unit::TestCase
+class Test_Calculator < Test::Unit::TestCase
  
   def test_simple
-    assert_equal(4, SimpleNumber.new(2).add(2) )
-    assert_equal(6, SimpleNumber.new(2).multiply(3) )
-    assert_equal(9,SimpleNumber.new(3).multiply(3))
-    assert_equal(3,SimpleNumber.new(9).divide(3))
+    assert_equal(4, Calculator.new(2).add(2) )
+    assert_equal(6, Calculator.new(2).multiply(3) )
+    assert_equal(9, Calculator.new(3).multiply(3))
+    assert_equal(3, Calculator.new(9).divide(3))
   end
  
 end
